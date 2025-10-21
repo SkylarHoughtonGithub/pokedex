5f197f676a65b7ebc79e50dd1e90a9f5e22d22b2
Skylar Houghton <skylarhoughtongithub@gmail.com>
2025-10-20 20:33:32 -0500

    add command struct and help, exit

--- main.go
+++ main.go
@@ -1,9 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
+
+	commands["map"] = cliCommand{
+		name:        "map",
+		description: "Displays a map of Pokedex locations",
+		callback:    commandHelp,
+	}
+
+	commands["bmap"] = cliCommand{
+		name:        "map",
+		description: "Displays a previous page of map of Pokedex locations",
+		callback:    commandHelp,
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+
+		line := scanner.Text()
+		cleanedLine := cleanInput(line)
+		if len(cleanedLine) == 0 {
+			continue
+		}
+
+		cmd, exists := commands[cleanedLine[0]]
+		if exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("read error:", err)
+	}
 }
